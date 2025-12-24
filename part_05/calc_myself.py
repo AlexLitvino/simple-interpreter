37ad99c11552d04ffa289fa3a668b7925502cfd4
AlexLitvino <litvinov.aleks@gmail.com>
2025-12-24 18:28:37 +0200

    self culc in progress

---
+++ part_05/calc_myself.py
@@ -0,0 +1,116 @@
+INTEGER, ADD, SUB, MLP, DIV, EOF = 'INTEGER', 'ADD', 'SUB', 'MLP', 'DIV', 'EOF'
+
+class Token:
+
+    def __init__(self, type, value):
+        self.type = type
+        self.value = value
+
+
+class Lexer:
+
+    def __init__(self, text):
+        self.text: str = text
+        self.pos = 0
+        self.current_symbol: str = self.text[self.pos]
+
+    def advance(self):
+        self.pos += 1
+        if self.pos == len(self.text):
+            self.current_symbol = None
+        else:
+            self.current_symbol = self.text[self.pos]
+
+    def skip_spaces(self):
+        while self.current_symbol is not None and self.current_symbol.isspace():
+            self.advance()
+
+    def integer(self):
+        integer_str = ''
+        while self.current_symbol.isdigit():
+            integer_str += self.current_symbol
+            self.advance()
+        return int(integer_str)
+
+    def error(self):
+        raise Exception('Invalid symbol')
+
+    def get_next_token(self):
+
+        while self.current_symbol is not None:
+
+            if self.current_symbol.isspace():
+                self.skip_spaces()
+                continue
+
+            if self.current_symbol.isdigit():
+                return Token(INTEGER, self.integer())
+
+            if self.current_symbol == '+':
+                return Token(ADD, '+')
+
+            if self.current_symbol == '-':
+                return Token(SUB, '-')
+
+            if self.current_symbol == '*':
+                return Token(MLP, '*')
+
+            if self.current_symbol == '/':
+                return Token(DIV, '/')
+
+            self.error()
+
+        return Token(EOF, None)
+
+
+class Interpreter:
+
+    def __init__(self, lexer: Lexer):
+        self.lexer = lexer
+        self.current_token = lexer.get_next_token()
+
+    def error(self):
+        raise Exception('Parsing error')
+
+    def eat(self, expected_type):
+        if self.current_token.type != expected_type:
+            self.error()
+        else:
+            self.current_token = self.lexer.get_next_token()
+
+    def factor(self):
+        result = self.current_token
+        self.eat(INTEGER)
+        return result.value
+
+    def term(self):
+        result = self.factor()
+        while self.current_token.type in (MLP, DIV):
+            if self.current_token.type == MLP:
+                self.eat(MLP)
+                result *= self.factor()
+                self.eat(INTEGER)
+            elif self.current_token.type == DIV:
+                self.eat(DIV)
+                result //= self.factor()
+                self.eat(INTEGER)
+        return result
+
+    def expr(self):
+        result = self.term()
+        while self.current_token.type in (ADD, SUB):
+            if self.current_token.type == ADD:
+                self.eat(ADD)
+                result += self.term()
+                self.eat(INTEGER)
+            elif self.current_token.type == SUB:
+                self.eat(SUB)
+                result -= self.term()
+                self.eat(INTEGER)
+        return result
+
+
+lexer = Lexer('1 + 2 * 3')
+interpreter = Interpreter(lexer)
+result = interpreter.expr()
+print(result)
